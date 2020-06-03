6d05fe952021010b429af21ba6539378becc02d0
winstxnhdw <56998716+winstxnhdw@users.noreply.github.com>
2020-06-03 11:55:57 +0800

    Added oop

--- Auto-IdleRPG.py
+++ Auto-IdleRPG.py
@@ -1,70 +1,94 @@
 from pynput.keyboard import Controller, Key
-import math, time
+import math, time, os
 
-keyboard = Controller()
+def main():
 
-mode = input("Adventure mode (1 - 30): ")
-boost = input("Do you have a time booster enabled (y/n)? ")
-if boost == "y":
-    fulltime = float(mode)/2
-if boost == "n":
-    fulltime = float(mode)
+    mode = input("Adventure mode (1 - 30): ")
+    if 1 <= int(mode) <= 30:
+        booster(int(mode))
 
-period = fulltime * 3600
-halftime = fulltime * 3600 / 2
+    else:
+        os.system('cls')
+        print("Invalid input.\n")
+        main()
 
-print('\nPeriod (T) of adventure: ', period, ' second(s)')
-print('T/2 of adventure: ', halftime, 'second(s)\n')
+def booster(mode):
+    boost = input("Do you have a time booster enabled (y/n)? ")
 
-adventure_mode = '$adventure ' + mode
-halftime_message = '[BOT] '+ str(halftime / 3600) + ' hours have passed since @wins started their adventure.'
+    if boost == "y":
+        totaltime = float(mode/2)
+        body(totaltime, mode)
 
-try:
-	print('5')
-	time.sleep(1)
-	print('4')
-	time.sleep(1)
-	print('3')
-	time.sleep(1)
-	print('2')
-	time.sleep(1)
-	print('1')
-	time.sleep(1)
-	
-	print('The adventure has begun')
+    elif boost == "n":
+        totaltime = float(mode)
+        body(totaltime, mode)
 
-	while True:		
-		keyboard.type('[BOT] This is an automatic IdleRPG message bot made by @wins.')
-		keyboard.press(Key.enter)
-		keyboard.release(Key.enter)
+    else:
+        print("Invalid input.\n")
+        booster()
 
-		keyboard.type(adventure_mode)
-		keyboard.press(Key.enter)
-		keyboard.release(Key.enter)
+def body(fulltime, mode):
+    keyboard = Controller()
 
-		time.sleep(4)
-		keyboard.type('$status')
-		keyboard.press(Key.enter)
-		keyboard.release(Key.enter)
+    period = fulltime * 3600
+    halftime = fulltime * 3600 / 2
 
-		time.sleep(4)
-		keyboard.type('$xp')
-		keyboard.press(Key.enter)
-		keyboard.release(Key.enter)
+    print('\nPeriod (T) of adventure: ', period, ' second(s)')
+    print('T/2 of adventure: ', halftime, 'second(s)\n')
 
-		time.sleep(halftime - 8)
-		print(halftime_message)
-		keyboard.type(halftime_message)
-		keyboard.press(Key.enter)
-		keyboard.release(Key.enter)
+    adventure_mode = '$adventure ' + str(mode)
+    halftime_message = '[BOT] '+ str(halftime / 3600) + ' hours have passed since @wins started their adventure.'
 
-		time.sleep(halftime + 3)
-		print('The adventure ended.')
-		keyboard.type('$status')
-		keyboard.press(Key.enter)
-		keyboard.release(Key.enter)
+    try:
+        print('5')
+        time.sleep(1)
+        print('4')
+        time.sleep(1)
+        print('3')
+        time.sleep(1)
+        print('2')
+        time.sleep(1)
+        print('1')
+        time.sleep(1)
 
-		time.sleep(5)
+        print('The adventure has begun')
 
-except KeyboardInterrupt:
-    pass
+        while True:		
+            keyboard.type('[BOT] This is an automatic IdleRPG message bot made by @wins.')
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+
+            keyboard.type(adventure_mode)
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+
+            time.sleep(4)
+            keyboard.type('$status')
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+
+            time.sleep(4)
+            keyboard.type('$xp')
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+
+            time.sleep(halftime - 8)
+            print(halftime_message)
+            keyboard.type(halftime_message)
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+
+            time.sleep(halftime + 3)
+            print('The adventure ended.')
+            keyboard.type('$status')
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+
+            time.sleep(5)
+
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == '__main__':
+    main()
+
